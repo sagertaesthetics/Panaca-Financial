8a69a80cf53edfb438100ed15562fc73606a12a3
sagertaesthetics <sagertaesthetics@gmail.com>
2026-04-28 01:32:59 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
+import "./styles/index.css";
+import "./styles/tailwind.css";
+import "./styles/theme.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
